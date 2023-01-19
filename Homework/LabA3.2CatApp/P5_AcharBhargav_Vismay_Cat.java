3db6a6b977a8b8bc15ade75bc0ba8202612fe34f
Vismay Bhargav <bhaavis1@gmail.com>
2023-01-18 19:27:37 -0800

    new lab

---
+++ Homework/LabA3.2CatApp/P5_AcharBhargav_Vismay_Cat.java
@@ -0,0 +1,24 @@
+public class P5_AcharBhargav_Vismay_Cat{
+    private String catName;
+    private int catAge;
+    private double catWeight;
+    private boolean isDomesticated;
+    private double catHunger;
+    public static double defaultCatWeight = 0.24;
+    private static final double DEFAULT_HUNGER = 0.25;
+    
+    public P5_AcharBhargav_Vismay_Cat(){
+        catName = "Tom";
+        catAge = 0;
+        catWeight = defaultCatWeight;
+        isDomesticated = false;
+        catHunger = DEFAULT_HUNGER;
+    }
+    
+    public P5_AcharBhargav_Vismay_Cat(String name, int age, double weight, boolean Domesticated){
+        catName = name;
+        catAge = age;
+        catWeight = weight;
+        isDomesticated = i
+    }
+}
