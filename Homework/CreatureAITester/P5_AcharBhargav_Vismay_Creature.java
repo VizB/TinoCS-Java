2601c3a4d5a06db3425e980fdd6e9f07c91d523b
Vismay Achar Bhargav <vismay.bhargav@gmail.com>
2023-03-05 23:23:11 -0800

    hahahaha

---
+++ Homework/CreatureAITester/P5_AcharBhargav_Vismay_Creature.java
@@ -0,0 +1,41 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.List;
+
+/**
+ * Represents a creature that moves toward treats and eats them.
+ * 
+ * @author Vismay Achar Bhargav 
+ * @version 3/5/2023
+ */
+public class P5_AcharBhargav_Vismay_Creature extends Actor {
+    public int speed = 2;
+    public Treat target = null;
+    
+    public void act() {
+        Actor treatLocated = getOneIntersectingObject(Treat.class);
+        boolean possibleWall = isTouching(Wall.class);
+        Actor insideWall = getOneIntersectingObject(Wall.class);
+        List<Treat> treats = getObjectsInRange(9999, Treat.class);
+        
+        if(!possibleWall){
+            if(treats.size() >= 1){
+               Treat firstTreat = treats.get(0); 
+               if(target == null){
+                target = firstTreat;
+                }
+            }
+        
+            if(target != null && possibleWall == false && insideWall == null){
+                turnTowards(target.getX(), target.getY());
+                move(speed);
+                setRotation(0);
+                if(treatLocated != null){
+                    getWorld().removeObject(treatLocated);
+                    target = null;
+                }
+            }
+        }
+        
+        
+    }
+}
