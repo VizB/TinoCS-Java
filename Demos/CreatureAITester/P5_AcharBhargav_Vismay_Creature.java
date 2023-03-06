0a27bed21ad98849a52bc923e01b75d8547cbf14
Vismay Bhargav <bhaavis1@gmail.com>
2023-03-05 16:29:44 -0800

    hfh

---
+++ Demos/CreatureAITester/P5_AcharBhargav_Vismay_Creature.java
@@ -0,0 +1,13 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class PX_LastName_FirstName_Creature here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class P5_AcharBhargav_Vismay_Creature extends Actor {
+    public void act() {
+        
+    }
+}
