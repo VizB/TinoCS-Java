a63ae1b86271f9f92eca58db0621dfa92f4cf7ab
Vismay Bhargav <bhaavis1@gmail.com>
2023-03-08 18:01:07 -0800

    stuff

--- Demos/CreatureAITester/P5_AcharBhargav_Vismay_Creature.java
+++ Demos/CreatureAITester/P5_AcharBhargav_Vismay_Creature.java
@@ -2,14 +2,14 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 import java.util.List;
 
 /**
- * Represents a creature that wanders around and collects treats.
+ * Represents a creature that moves toward treats and eats them.
  * 
- * @author Vismay Achar Bhargav
- * @version (a version number or a date)
+ * @author Vismay Achar Bhargav 
+ * @version 3/5/2023
  */
 public class P5_AcharBhargav_Vismay_Creature extends Actor {
     public int speed = 2;
-    public Treat target = null;
+    Actor closestTreat = null;
     
     public void act() {
         Actor treatLocated = getOneIntersectingObject(Treat.class);
@@ -17,23 +17,61 @@ public class P5_AcharBhargav_Vismay_Creature extends Actor {
         Actor insideWall = getOneIntersectingObject(Wall.class);
         List<Treat> treats = getObjectsInRange(9999, Treat.class);
         
-        if(!possibleWall){
-            if(treats.size() >= 1){
-               Treat firstTreat = treats.get(0); 
-               if(target == null){
-                target = firstTreat;
-                }
-            }
+        if(closestTreat == null){
+            closestTreat = closestReachableTreat();
+        }
         
-            if(target != null && possibleWall == false && insideWall == null){
-                turnTowards(target.getX(), target.getY());
+        if(closestTreat != null) {
+            if(!isTouching(Wall.class)){
+                turnTowards(closestTreat.getX(), closestTreat.getY());
                 move(speed);
                 setRotation(0);
-                if(treatLocated != null){
-                    getWorld().removeObject(treatLocated);
-                    target = null;
-                }
             }
+            if(treatLocated != null){
+                getWorld().removeObject(treatLocated);
+                closestTreat = null;
+            }
+        }
+    }
+    
+    public int numStepsToTreat(Treat treat) {
+        int moves = 0;
+        int initialX = getX();
+        int initialY = getY();
+        while (getOneIntersectingObject(Treat.class) == null){
+            if(isTouching(Wall.class)) {
+                setLocation(initialX, initialY);
+                setRotation(0);
+                System.out.println("invalid move");
+                return -1;
+            }
+            turnTowards(treat.getX(), treat.getY());
+            move(speed);
+            setRotation(0);
+            moves++;
+        }
+        setLocation(initialX, initialY);
+        setRotation(0);
+        return moves;
+    }
+    
+    public Actor closestReachableTreat() {
+        List<Treat> treats = getObjectsInRange(9999, Treat.class);
+        Treat closeTreat;
+        if(treats.size() >= 1){
+            closeTreat = treats.get(0);
+        } else {
+            closeTreat = null;
+        }
+        for(int i = 0; i < treats.size(); i++) {
+            if(numStepsToTreat(treats.get(i)) != -1 && numStepsToTreat(treats.get(i)) < numStepsToTreat(closeTreat)) {
+                closeTreat = treats.get(i);
+            }
+        }
+        if(treats.size() < 1){
+            return null;
+        } else {
+            return closeTreat;
         }
     }
 }
